7b9744d49f16db6f8fe390bd57b1d706eb6e7ac9
ialmeida <ialmeida.pb@gmail.com>
2021-04-12 18:07:13 -0300

    done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items=[];
+    this.length=0;
+  }
+ 
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    this.items.sort((a, b) => (a - b));
+  }
 
-  add(item) {}
+  get(pos) {
+    if(!this.items[pos]){   //para acessar indice de uma array a posicao/referencia deve estar entre chaves. 
+    throw new Error('OutOfBounds');
+    }else
+    return this.items[pos];
+  }
 
-  get(pos) {}
+  max() {
+  if(this.items.length === 0){
+    throw new Error('EmptySortedList');
+  } else{
+    return Math.max(...this.items)
+  }
+  }
 
-  max() {}
+  min() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    } else{
+      return Math.min(...this.items)
+    }
+  }
 
-  min() {}
+  sum() {
+  if(this.items.length!=0){
+    return this.items.reduce((a, b) => a + b)
+  } else{
+    return 0;
+  }
 
-  sum() {}
+}
 
-  avg() {}
+  avg() {
+    return this.items.reduce((a, b) => a + b)/this.items.length
+  }
 }
 
 module.exports = SortedList;
